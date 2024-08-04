adeec36ba912970cfd585a95a6d2957352743dc0
Lesly Samaritano Ayala <1628112@utp.edu.pe>
2024-08-04 16:51:56 -0500

    feat: validación del password.dart

--- lib/features/authentication/infraestructura/inputs/password.dart
+++ lib/features/authentication/infraestructura/inputs/password.dart
@@ -1,24 +1,30 @@
 import 'package:formz/formz.dart';
 
-enum PasswordError { empty, length }
+enum PasswordError { empty, length, character }
 
 class Password extends FormzInput<String, PasswordError> {
   const Password.pure() : super.pure('');
   const Password.dirty(super.value) : super.dirty();
 
+  static final _rePasswordRegExp = RegExp(r'^[A-Za-z\d@$!%*?&]{8,}$');
+
   String? get errorMessage {
     if (isValid || isPure) return null;
 
     if (displayError == PasswordError.empty) return 'The field is required.';
-    if (displayError == PasswordError.length) return 'Minimum 6 characters.';
-    
+    if (displayError == PasswordError.length) return 'Minimum 8 characters.';
+    if (displayError == PasswordError.character) {
+      return 'Includes a special character.';
+    }
+
     return null;
   }
 
   @override
   PasswordError? validator(String value) {
     if (value.isEmpty || value.trim().isEmpty) return PasswordError.empty;
-    if (value.length < 6) return PasswordError.length;
+    if (value.length < 8) return PasswordError.length;
+    if (!_rePasswordRegExp.hasMatch(value)) return PasswordError.character;
 
     return null;
   }
