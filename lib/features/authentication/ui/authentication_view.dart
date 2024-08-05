dba3076cc6a87a74e6be2df4247351af3bb7b79b
Lesly Samaritano Ayala <1628112@utp.edu.pe>
2024-08-05 18:17:09 -0500

    feat: snackbars error y exitoso

--- lib/features/authentication/ui/authentication_view.dart
+++ lib/features/authentication/ui/authentication_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
 import 'package:guardowl/constants/constants.dart';
 import 'package:guardowl/features/authentication/blocs/auth/auth_cubit.dart';
 import 'package:guardowl/features/authentication/blocs/login/login_cubit.dart';
@@ -7,6 +8,7 @@ import 'package:guardowl/features/authentication/blocs/sign_up/sign_up_cubit.dar
 import 'package:guardowl/features/authentication/ui/register_view.dart';
 import 'package:guardowl/features/authentication/ui/widgets/widgets_auhtentication.dart';
 import 'package:guardowl/features/share/share.dart';
+import 'package:guardowl/utils/snackbar_alert.dart';
 
 class AuthenticationView extends StatefulWidget {
   const AuthenticationView({super.key});
@@ -19,7 +21,7 @@ class _AuthenticationViewState extends State<AuthenticationView> {
   bool isRegisterMode = true;
 
   void toggleAuthMode() {
-    setState(() { 
+    setState(() {
       isRegisterMode = !isRegisterMode;
     });
   }
@@ -34,100 +36,116 @@ class _AuthenticationViewState extends State<AuthenticationView> {
     final logInCubit = context.read<LogInCubit>();
     final signUpCubit = context.read<SignUpCubit>();
 
-    return BlocBuilder<AuthCubit, AuthState>(
-      builder: (context, state) {
-        if (state.isSignedIn) {
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            Navigator.pushReplacementNamed(context, '/home');
-          });
-          return CircularProgressIndicator(color: color.primary);
-        } else {
-          return Scaffold(
-            body: ListView(
-              children: [
-                MyBackground(
-                  height: 1.4,
-                  child: Center(
-                    child: HeaderAuthentication(
-                        titleAuthentication:
-                            isRegisterMode ? 'Sing In' : 'Sign Up',
-                        subTitleAuthentication: isRegisterMode
-                            ? 'Hi! Welcome back, you have been missed'
-                            : 'Create your new account'),
-                  ),
-                ),
-                isRegisterMode ? const SingInView() : const RegisterView(),
-                const SizedBox(height: 20),
-                //button home
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: paddingAppBar),
-                  child: FilledButton(
-                    style: singInStyleButton,
-                    onPressed: () {
-                      if (isRegisterMode) {
-                        print(
-                            "Button pressed, isRegisterMode: $isRegisterMode");
-                        logInCubit.validtatingInputsLogIn();
-                        logInCubit.logInWithCredentials();
-                      } else {
-                        signUpCubit.validtatingInputsSignUp();
-                        signUpCubit.signUpWithCredentials();
-                      }
-                    },
-                    child: const Text('Continue'),
-                  ),
-                ),
-                const SizedBox(height: 30),
-                const _LineOtherSocial(),
-                const SizedBox(height: 35),
-                //butons of social
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    _ButtonSocialNetwork(
-                      iconGoogle,
-                      onPressed: () {
-                        if (isRegisterMode) {
-                          print(
-                              "Button Google, isRegisterMode: $isRegisterMode");
-                          logInCubit.logInWithGoogle();
-                          //todo: login to Apple
-                        } else {
-                          signUpCubit.signUpWithGoogle();
-                          //todo: signUp to Apple
-                        }
-                      },
-                    ),
-                    const SizedBox(width: 30),
-                    _ButtonSocialNetwork(
-                      iconApple,
-                      onPressed: () {
-                        if (isRegisterMode) {
-                          print(
-                              "Button Apple, isRegisterMode: $isRegisterMode");
-                          //todo: login to Apple
-                        } else {
-                          //todo: signUp to Apple
-                        }
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 35),
-                LinkAccount(
-                  routeName: isRegisterMode ? 'up' : 'in',
-                  questionText: isRegisterMode
-                      ? 'Don’t have an account?'
-                      : 'If you have an account?',
-                  onpressed: toggleAuthMode,
-                ),
-                const SizedBox(height: 35),
-              ],
+    return BlocListener<LogInCubit, LogInState>(
+      listener: (context, state) {
+        if (state.status == FormzSubmissionStatus.success) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            snackBarWhenSuccess(color: color.primary),
+          );
+        } else if (state.status == FormzSubmissionStatus.failure) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            snackBarWhenFailure(
+              snackBarFailureText: state.exceptionError,
+              color: color.error,
             ),
           );
         }
       },
+      child: BlocBuilder<AuthCubit, AuthState>(
+        builder: (context, state) {
+          if (state.isSignedIn) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              Navigator.pushReplacementNamed(context, '/home');
+            });
+            return CircularProgressIndicator(color: color.primary);
+          } else {
+            return Scaffold(
+              body: ListView(
+                children: [
+                  MyBackground(
+                    height: 1.4,
+                    child: Center(
+                      child: HeaderAuthentication(
+                          titleAuthentication:
+                              isRegisterMode ? 'Sing In' : 'Sign Up',
+                          subTitleAuthentication: isRegisterMode
+                              ? 'Hi! Welcome back, you have been missed'
+                              : 'Create your new account'),
+                    ),
+                  ),
+                  isRegisterMode ? const SingInView() : const RegisterView(),
+                  const SizedBox(height: 20),
+                  //button home
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: paddingAppBar),
+                    child: FilledButton(
+                      style: singInStyleButton,
+                      onPressed: () {
+                        if (isRegisterMode) {
+                          print(
+                              "Button pressed, isRegisterMode: $isRegisterMode");
+                          logInCubit.validtatingInputsLogIn();
+                          logInCubit.logInWithCredentials();
+                        } else {
+                          signUpCubit.validtatingInputsSignUp();
+                          signUpCubit.signUpWithCredentials();
+                        }
+                      },
+                      child: const Text('Continue'),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  const _LineOtherSocial(),
+                  const SizedBox(height: 35),
+                  //butons of social
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _ButtonSocialNetwork(
+                        iconGoogle,
+                        onPressed: () {
+                          if (isRegisterMode) {
+                            print(
+                                "Button Google, isRegisterMode: $isRegisterMode");
+                            logInCubit.logInWithGoogle();
+                            //todo: login to Apple
+                          } else {
+                            signUpCubit.signUpWithGoogle();
+                            //todo: signUp to Apple
+                          }
+                        },
+                      ),
+                      const SizedBox(width: 30),
+                      _ButtonSocialNetwork(
+                        iconApple,
+                        onPressed: () {
+                          if (isRegisterMode) {
+                            print(
+                                "Button Apple, isRegisterMode: $isRegisterMode");
+                            //todo: login to Apple
+                          } else {
+                            //todo: signUp to Apple
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 35),
+                  LinkAccount(
+                    routeName: isRegisterMode ? 'up' : 'in',
+                    questionText: isRegisterMode
+                        ? 'Don’t have an account?'
+                        : 'If you have an account?',
+                    onpressed: toggleAuthMode,
+                  ),
+                  const SizedBox(height: 35),
+                ],
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
