49072aeda14a99099aca943af89ab923d425246c
Christopher Bobadilla Plasencia <80471939+Criszx17Dev@users.noreply.github.com>
2024-08-10 02:16:14 -0400

    fix: navegacion, chat con gemini, creacion de modelos

---
+++ lib/features/assistant/widgets/assistant_profile_headder.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:guardowl/features/authentication/blocs/auth/auth_cubit.dart';
+import 'package:guardowl/features/share/button_assistant.dart';
+
+class AssistantProfileHeader extends StatelessWidget {
+  const AssistantProfileHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+
+    final textTitleLargeAlert = textTheme.titleLarge?.copyWith(
+      color: color.primary,
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+    );
+
+    return Column(
+      children: [
+        Row(
+          children: [
+            const Hero(
+              tag: '/assistant-button',
+              child: ButtonAssistant(),
+            ),
+            const SizedBox(width: 16.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "GuardOwl",
+                  style: textTitleLargeAlert,
+                ),
+                Text(
+                  "Your assitant",
+                  style: textTheme.bodyMedium?.copyWith(
+                    color: color.outlineVariant,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        const SizedBox(height: 24.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      'Hi',
+                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      width: 8.0,
+                    ),
+                    Center(
+                      child: BlocConsumer<AuthCubit, AuthState>(
+                          listener: (context, state) => state.currentUser,
+                          builder: (context, state) {
+                            return Container(
+                              padding: const EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+                                color: color.primaryContainer,
+                              ),
+                              child: Text(
+                                '@${state.fullName}',
+                                style: TextStyle(
+                                  color: color.primary,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            );
+                          }),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16.0),
+                const Text(
+                  'Welcome! 🎉',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 12.0),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: const Text(
+                    'My name is GuardOwl. I’m 🤖 designed to guide you during your trip. I will show you the safety places:',
+                    style: TextStyle(fontSize: 14),
+                    softWrap: true,
+                    overflow: TextOverflow.clip,
+                  ),
+                )
+              ],
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
---
+++ lib/features/assistant/widgets/chat_messages_list.dart
@@ -0,0 +1,78 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:guardowl/features/assistant/models/chat_message.dart';
+import 'package:guardowl/features/authentication/blocs/auth/auth_cubit.dart';
+
+class ChatMessagesList extends StatelessWidget {
+  const ChatMessagesList({super.key, required this.controller});
+
+  final ScrollController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AuthCubit, AuthState>(
+        listener: (context, state) => state.currentUser,
+        builder: (context, state) {
+          return StreamBuilder<List<ChatMessage>>(
+            stream: fetchMessages(),
+            builder: (ctx, AsyncSnapshot<List<ChatMessage>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              }
+              final messages = snapshot.data!;
+
+              return ListView.builder(
+                shrinkWrap: true,
+                controller: controller,
+                itemCount: messages.length,
+                itemBuilder: (ctx, index) {
+                  final message = messages[index];
+
+                  final isUserMessage = message.userId == state.currentUser?.uid;
+                  final (left, right) = (isUserMessage ? 25.0 : 0.0, isUserMessage ? 0.0 : 25.0);
+                  return Padding(
+                    padding: EdgeInsets.only(top: 7, bottom: 7, left: left, right: right),
+                    child: ListTile(
+                      title: Text(message.message),
+                      subtitle: isUserMessage ? const Text('You') : const Text('Guardowl AI'),
+                      tileColor: isUserMessage ? Colors.blue[100] : Colors.grey[200],
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        });
+  }
+}
+
+Stream<List<ChatMessage>> fetchMessages() {
+  final firestore = FirebaseFirestore.instance;
+  try {
+    final messages = firestore
+        .collection('messages')
+        .orderBy('createdAt')
+        // .where(
+        //   'destination',
+        //   isEqualTo: 'Lima',
+        // )
+        .withConverter<ChatMessage>(
+          fromFirestore: (snapshot, _) => ChatMessage.fromFirebaseUser(
+            snapshot.data()!,
+          ),
+          toFirestore: (value, _) => value.toMapFirestore(),
+        )
+        .snapshots()
+        .map((snapshot) => snapshot.docs)
+        .map((docs) => docs.map((doc) => doc.data()))
+        .map((messages) => messages.toList());
+
+    return messages;
+  } catch (e) {
+    return throw Exception('Failed to load data');
+  }
+}
